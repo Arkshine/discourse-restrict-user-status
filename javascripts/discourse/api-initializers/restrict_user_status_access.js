1ec661f7052f0564618b5f0b31f9ba11615d40ca
Arkshine <github@arkshine.me>
2023-03-30 04:20:19 +0200

    Check site settings

--- javascripts/discourse/api-initializers/restrict_user_status_access.js
+++ javascripts/discourse/api-initializers/restrict_user_status_access.js
@@ -6,6 +6,13 @@ import discourseComputed from "discourse-common/utils/decorators";
 const PLUGIN_ID = "restrict-user-status";
 
 export default apiInitializer("0.11.1", (api) => {
+  const siteSettings = api.container.lookup("service:site-settings");
+  const currentUser = api.container.lookup("service:current-user");
+
+  if (!siteSettings.enable_user_status || !currentUser) {
+    return;
+  }
+
   const groupsIds = settings.allowed_groups.split("|").map((id) => Number(id));
 
   // If everyone group, don't go further.
@@ -40,25 +47,35 @@ export default apiInitializer("0.11.1", (api) => {
     },
   });
 
-  api.modifyClass("component:user-menu/menu-tab", {
-    pluginId: PLUGIN_ID,
+  /**
+   * Removes the status in the new notification menu.
+   * Without altering the template, I can't see others solutions.
+   */
+  if (
+    siteSettings.navigation_menu !== "legacy" ||
+    siteSettings.enable_new_notifications_menu
+  ) {
+    api.modifyClass("component:user-menu/menu-tab", {
+      pluginId: PLUGIN_ID,
 
-    removeUserStatus() {
-      document.querySelector("li.set-user-status")?.remove();
-    },
+      removeUserStatus() {
+        document.querySelector("li.set-user-status")?.remove();
+      },
 
-    get isActive() {
-      const isActive = this.args.tab.id === this.args.currentTabId;
+      get isActive() {
+        const isActive = this.args.tab.id === this.args.currentTabId;
 
-      if (
-        isActive &&
-        this.args.currentTabId === "profile" &&
-        !userAllowed(getOwner(this).lookup("service:current-user")?.groups)
-      ) {
-        scheduleOnce("afterRender", this, "removeUserStatus");
-      }
+        if (
+          isActive &&
+          this.args.currentTabId === "profile" &&
+          !userAllowed(getOwner(this).lookup("service:current-user"))
+        ) {
+          scheduleOnce("afterRender", this, "removeUserStatus");
+        }
 
-      return isActive;
-    },
-  });
+        return isActive;
+      },
+    });
+  }
+
 });
